197c78d45a91bd6f67491962b4f679b6ba4bd099
Evgeniy652 <evgen652@mail.ru>
2022-08-20 12:53:07 +0300

    feat: add login form

---
+++ src/index.js
@@ -0,0 +1,20 @@
+window.onload = function () {
+	const login = document.getElementById('login');
+	const flip = document.querySelector('.flip');
+	const accountBlock = document.querySelector('.account-block');
+
+	accountBlock.addEventListener('click', (event) => {
+		login.classList.toggle('login-visible');
+	})
+
+	login.addEventListener('click', (event) => {
+		if(event.target.className === 'button-cancel-form') {
+			login.classList.remove('login-visible');
+		}
+
+		if(event.target.className === 'flipper') {
+			flip.classList.toggle('flipped');
+		}
+	})
+
+}
