e1b2f15818771e1b1665a02dc487b1528532b75b
Evgeniy652 <evgen652@mail.ru>
2022-08-28 14:14:50 +0300

    feat:add card img

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,7 @@ import { LoginService } from './components/common/login';
 import { HomeService } from './components/pages/home';
 import Sprint from './components/pages/sprint';
 import { StatisticsService } from './components/pages/statistics';
+import { TextbookService } from './components/pages/textbook';
 
 import './style.scss';
 
@@ -16,18 +17,19 @@ class Main {
     const home = new HomeService();
     const statistic = new StatisticsService();
     const sprint = new Sprint();
-		const footer = new FooterService();
+    const footer = new FooterService();
+    const textbook = new TextbookService();
 
     Main.commonComponents();
 
     router
       .on('', () => {
         home.start();
-				footer.start();
+        footer.start();
       })
       .on('/statistics', () => {
         statistic.start();
-				footer.start();
+        footer.start();
       })
       .on('/sprint/:group/:page', ({ data }) => {
         const group = Number.isInteger(Number(data.group)) ? Number(data.group) : undefined;
@@ -37,13 +39,16 @@ class Main {
       .on('/sprint/?', () => {
         sprint.start();
       })
+      .on('/textbook', () => {
+        textbook.start();
+        footer.start();
+      })
       .resolve();
   }
 
   private static commonComponents() {
     const header = new HeaderService();
     const login = new LoginService();
-   
 
     login.start();
     header.start();
