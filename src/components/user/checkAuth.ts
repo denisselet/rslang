f03a7ebb5537870893117d3a6ae3f0501dce0155
denisselet <den8rr@gmail.com>
2022-09-04 00:52:04 +0300

    fix: add import

--- src/components/user/checkAuth.ts
+++ src/components/user/checkAuth.ts
@@ -1,3 +1,5 @@
+import UsersService from "../services/users-service";
+
 export function checkAuth() {
   if (localStorage.getItem('user')) {
     if ((+localStorage.getItem('dateTimeAuth') + 14400000) < new Date().getTime()) {
